68b5e9e37bf3d6d733b209498265024cdc976d87
nemostatus <mattcorralejo1@gmail.com>
2021-04-17 05:36:36 -0700

    adding methods into prototype and validating the supported languages

--- Greetr.js
+++ Greetr.js
@@ -3,7 +3,34 @@
       return new Greetr.init(firstName,lastName,language)
   } //coding it this way so the new keyword doesnt have to be used all the time
 
-  Greetr.prototype = {}; //prototype of all object below
+  var supportedLanguages = ["en", "es"] //hidden to outside world, uses closure
+
+  var greetings ={
+en: 'Hello',
+es: 'Hola'
+  }; 
+
+  var formalGreetings ={
+en: 'greetings',
+es: 'saludos'
+  };
+
+  var logMessages ={
+      en: 'Logged In',
+      es: 'Inicio sesion'
+  }
+  Greetr.prototype = {
+fullName: function(){
+    return this.firstName + ' ' + this.lastName;
+},
+
+validate: function(){
+    if(supportedLanguages.indexOf(this.language) === -1){
+throw "invalid message"
+    }
+} //throws error if language isnt found in supported languages arr
+
+  }; //prototype of all object below, methods go here
 
   Greetr.init = function(firstName,lastname,language){
      var self = this  //dont have to worry about what this is later
